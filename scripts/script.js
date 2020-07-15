c7a98f2fdadd036d7561932399ef7fb8edfb1576
yuppiez <bluubs@DESKTOP-E9ECPKD.localdomain>
2020-07-15 15:35:11 -0700

    ETCH A SKETCH 1.0
    create btn element
    	addEventListener to button
    onlclick run createGrid with parameters as prompts
    	set totalSections to user input rows * user input columns
    run createGridDivs(totalsections)
    
    I AM SO HAPPY THIS WORKS. LETS GOOO

--- scripts/script.js
+++ scripts/script.js
@@ -13,24 +13,36 @@ document.body.appendChild(btn);
 btn.textContent = 'Alter Grid Size';
 
 
-/* btn.addEventListener('click' () => {
-  
+btn.addEventListener('click' , () => {
+  createGrid(prompt("How many rows do you want?"), prompt("How many columns do you want?"));
 })
 
 /* Create grid */
-/* container.style.display = 'grid';
+function createGrid(rows, columns) {
+
+
+
+container.style.display = 'grid';
 container.style.position = 'absolute';
 container.style.width = '100%'
-container.style.height = '80%' */ 
+container.style.height = '80%' 
+container.style.top = '15%'
 
+container.style['grid-template-rows'] = `repeat(${rows}, 1fr)`
+container.style['grid-template-columns'] = `repeat(${columns}, 1fr)`
+
+const totalSections = rows * columns;
+
+createGridDivs(totalSections)
+}
 
 /* Create grid divs  */
 
-createGridDivs();
-function createGridDivs(rows, columns) {
+
+function createGridDivs(totalSections) {
 
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < totalSections; i++) {
   /* loop scope */
   const gridSquare = document.createElement('div');
   gridSquare.className = 'grid-square';
